657ba452b7df136783e4167f2a32a42b7b2bea78
Yana Kozma <jane.kozma@gmail.com>
2025-01-23 12:39:49 +0200

    fix productDisplay component style

--- src/components/ProductDisplay.tsx
+++ src/components/ProductDisplay.tsx
@@ -6,10 +6,9 @@ import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../store/store.ts";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-
+import {Alert, AlertTitle} from "@mui/material";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 
 
 export default function ProductDisplay() {
@@ -38,46 +37,123 @@ export default function ProductDisplay() {
 
     if (error) {
         return (
-            <Typography align="center" variant="h6" color="error">
-                {error}
-            </Typography>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "80vh",
+                }}
+            >
+                <Alert
+                    severity="error"
+                    icon={<ErrorOutlineIcon />}
+                    sx={{
+                        maxWidth: 600,
+                        width: "100%",
+                        boxShadow: 3,
+                        p: 2,
+                        borderRadius: 2,
+                        textAlign: "center",
+                        bgcolor: "#f8d7da",
+                        color: "#721c24",
+                    }}
+                >
+                    <AlertTitle>Error</AlertTitle>
+                    {error || "An unexpected error occurred. Please try again later."}
+                </Alert>
+            </Box>
         );
     }
 
     if (!product) {
         return (
-            <Typography align="center" variant="h6" color="error">
-                Product not found
-            </Typography>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "80vh",
+                }}
+            >
+                <Alert
+                    severity="error"
+                    icon={<ErrorOutlineIcon />}
+                    sx={{
+                        maxWidth: 600,
+                        width: "100%",
+                        boxShadow: 3,
+                        p: 2,
+                        borderRadius: 2,
+                        textAlign: "center",
+                        bgcolor: "#f8d7da",
+                        color: "#721c24",
+                    }}
+                >
+                    <AlertTitle>Product Not Found</AlertTitle>
+                    We couldn't find the product you are looking for. Please check the URL or try again later.
+                </Alert>
+            </Box>
         );
     }
 
-
     return (
-        <Box sx={{display: "flex", justifyContent: "center", p: 4}}>
-            <Card sx={{maxWidth: 600, width: "100%", boxShadow: 3}}>
-                <CardMedia
-                    component="img"
-                    height="300"
-                    image={product.img}
-                    alt={product.name}
-                    sx={{objectFit: "contain"}}
-                />
-                <CardContent>
-                    <Typography variant="h5" component="div" gutterBottom>
-                        {product.name}
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                gap: 4,
+                p: 4,
+            }}
+        >
+            <CardMedia
+                component="img"
+                image={product.img}
+                alt={product.name}
+                sx={{
+                    width: 400,
+                    height: 400,
+                    objectFit: "contain",
+                    boxShadow: 3,
+                    borderRadius: 2,
+                }}
+            />
+            <Box sx={{display: "flex", flexDirection: "column", gap: 2}}>
+                <Typography variant="h5" component="div" gutterBottom>
+                    {product.name}
+                </Typography>
+
+                <Typography variant="h6" color="primary">
+                    Price: ${product.price}
+                </Typography>
+
+                <Box>
+                    <Typography variant="body1" color="text.secondary">
+                        <strong>Brand:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit
                     </Typography>
-                    <Typography variant="body1" color="text.secondary" gutterBottom>
-                        Category: {product.bsr_category}
+                    <Typography variant="body1" color="text.secondary">
+                        <strong>Color:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit
                     </Typography>
-                    <Typography variant="h6" color="primary">
-                        Price: ${product.price}
+                    <Typography variant="body1" color="text.secondary">
+                        <strong>Form Factor:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
+                    <Typography variant="body1" color="text.secondary">
                         <strong>ASIN:</strong> {product.asin}
                     </Typography>
-                </CardContent>
-            </Card>
+                </Box>
+                <Typography variant="body2" color="text.secondary">
+                    <strong>About this item:</strong>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consequuntur deleniti dolore
+                    dolorem, doloremque dolores eligendi iure maiores, obcaecati, placeat quasi quisquam reiciendis
+                    repellat saepe tempore. Ex nemo qui quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Culpa deserunt id magnam molestiae officia pariatur perferendis quas soluta voluptas! A debitis
+                    dolorem eos id illo iusto possimus qui quo reprehenderit. Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Accusantium aliquam asperiores assumenda consequuntur culpa distinctio dolor
+                    expedita explicabo, iusto laudantium magni, mollitia officiis quae quos repellendus sapiente sunt
+                    tenetur, ut?
+                </Typography>
+            </Box>
         </Box>
-    )
-};
+    );
+}
